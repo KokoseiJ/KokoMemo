2c3aa43ddda90f3e1eb9a321df82bc7285fd77b4
KokoseiJ <kokoseij@gmail.com>
2022-04-16 03:12:23 -0700

    Setup config loaders

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,31 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from config import test_setup
+
+import os
+
 db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
 
-    app.config.from_pyfile('config.py')
+    app.config.from_prefixed_env()
 
-    if test_config is None:
-        app.config.from_mapping(test_config)
+    if app.config['DEBUG']:
+        app.config.from_object("config.DebugConfig")
+    else:
+        app.config.from_object("config.ProdConfig")
+
+    if app.config['TESTING']:
+        app.config.from_object("config.TestConfig")
+
+    inst_config_path = os.path.join(app.instance_path, "config.py")
+
+    if os.path.exists(inst_config_path):
+        app.config.from_pyfile(inst_config_path)
 
     from . import views
     for viewname in views.__all__:
@@ -22,5 +36,8 @@ def create_app(test_config=None):
 
     db.init_app(app)
     migrate.init_app(app)
+
+    if app.config['TESTING']:
+        test_setup()
 
     return app
